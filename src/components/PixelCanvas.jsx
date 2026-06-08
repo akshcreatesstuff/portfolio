dde4986cd3b0dd0b7e430ce1b9ef746b025a77e5
Aishps <aishps@Aishpss-MacBook-Air.local>
2026-06-08 13:44:54 +0530

    fixed multi-device resolution bug

--- src/components/PixelCanvas.jsx
+++ src/components/PixelCanvas.jsx
@@ -22,7 +22,8 @@ function PixelCanvas() {
       ref={canvasRef}
       style={{
         width: "100%",
-        height: "100%"
+        height: "100%",
+        display: "block"
       }}
     />
   );
